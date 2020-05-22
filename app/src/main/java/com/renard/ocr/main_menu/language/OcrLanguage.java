d94df6b03e0a1f81ba7cff8933d931e2a8046fcd
shreyasssa <shreyasambekar2@gmail.com>
2020-05-22 22:55:53 +0530

    Version 4.0 traineddata files can now be downloaded from the app... No need to pack the required files in the apk

--- app/src/main/java/com/renard/ocr/main_menu/language/OcrLanguage.java
+++ app/src/main/java/com/renard/ocr/main_menu/language/OcrLanguage.java
@@ -67,7 +67,8 @@ public class OcrLanguage implements Parcelable {
     List<Uri> getDownloadUris() {
         List<Uri> result = new ArrayList<>();
         final String part2 = ".traineddata";
-        String networkDir = "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/";
+        //https://github.com/tesseract-ocr/tessdata/blob/master/hin.traineddata?raw=true
+        String networkDir = "https://github.com/tesseract-ocr/tessdata/raw/4.00/";
         result.add(Uri.parse(networkDir + getValue() + part2));
         return result;
     }
